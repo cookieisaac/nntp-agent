bd07b7c82d206b1e92a940b619b284bc9be5994a
Ike Ma <cookieisaac.zju@gmail.com>
2016-06-24 00:24:28 -0400

    Tested prototype 2

--- Readme.md
+++ Readme.md
@@ -12,5 +12,98 @@ server.list()
 server.group('gmane.comp.python.committers')
 ```
 
+## How to Merge Conflict
+
+Q: Why I cannot push my code?
+```
+# git push origin master
+Username for 'https://github.com': cookieisaac
+Password for 'https://cookieisaac@github.com':
+To https://github.com/cookieisaac/nntp-agent.git
+ ! [rejected]        master -> master (fetch first)
+error: failed to push some refs to 'https://github.com/cookieisaac/nntp-agent.git'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first merge the remote changes (e.g.,
+hint: 'git pull') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+
+```
+A: As described [here](http://stackoverflow.com/a/10298391/3806343),  the message tells you,
+
+Merge the remote changes (e.g. 'git pull')
+Use git pull to pull the latest changes from the remote repository to your local repository. In this case, pulling changes will require a merge because you have made changes to your local repository.
+
+I'll provide an example and a picture to explain. Let's assume your last pull from origin/branch was at Commit B. You have completed and committed some work (Commit C). At the same time, someone else has completed their work and pushed it to origin/branch (Commit D). There will need to be a merge between these two branches.
+```
+local branch:                         --- Commit C 
+                                    /
+                                   /
+                                  /
+origin/branch: Commit A ------ Commit B ---- Commit D
+```
+Because you are the one that wants to push, Git forces you to perform the merge. To do so, you must first pull the changes from origin/branch.
+```
+local branch:                         --- Commit C -- Commit E
+                                    /               /           
+                                   /               /             
+                                  /               /               
+origin/branch: Commit A ------ Commit B ---- Commit D 
+```
+After completing the merge, you will now be allowed to fast-forward origin/branch to Commit E by pushing your changes.
+
+Git requires that you handle merges yourself, because a merge may lead to conflicts.
+
+
+Q:I made a few changes on Github and then I made a few change in local. How do I push my local change to a updated Github?
+
+
+A: As suggested [here](https://githowto.com/resolving_conflicts), open the conlicted file, manually resolve the following part
+
+```
+<<<<HEAD
+Conflict A
+========
+Conflict B
+>>>>>ChangesetID
+```
+
+to 
+
+```
+Merged
+```
+
+Then 
+```
+git add lib/hello.html
+git commit -m "Merged master fixed conflict."
+```
+
+Q: What if I got the following message?
+```
+You have not concluded your merge (MERGE_HEAD exists).
+Please, commit your changes before you can merge.
+```
+
+A: As illustrated [here](http://stackoverflow.com/a/11647899/3806343)
+
+If your previous pull failed to merge automatically and went to conflict state. And the conflict wasn't resolved properly before the next pull.
+
+1)Undo the merge and pull again.
+
+To undo a merge:
+
+`git merge --abort` [Since git version 1.7.4]
+
+`git reset --merge` [prior git versions]
+
+2) Resolve the conflict.
+
+3) Don't forget to add and commit the merge.
+
+4) git pull now should work fine.
+
+
 ## Todo
 UML
--- newsagent.py
+++ newsagent.py
@@ -1,29 +1,144 @@
 from nntplib import NNTP
 from time import strftime, time, localtime
+from email import message_from_string
+from urllib import urlopen
+import textwrap
+import re
 
-day = 24 * 60 * 60
+day = 24 * 60 * 60 #Number of seconds in a day
 
-yesterday = localtime(time() - day)
-date = strftime('%y%m%d', yesterday)
-hour = strftime('%H%M%S', yesterday)
-
-servername = 'news-archive.icm.edu.pl'
-group = 'alt.pl.sztuki.walki'
-server = NNTP(servername)
-
-ids = server.newnews(group, date, hour)[1]
-
-for id in ids:
-    head = server.head(id)[3]
-    for line in head:
-        if line.lower().startswith('subject:'):
-            subject = line[9:]
-            break
+def wrap(string, max=70):
+    return '\n'.join(textwrap.wrap(string)) + '\n'
+    
+    
+class NewsItem:
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        
+class NewsAgent:
+    def __init__(self):
+        self.sources = []
+        self.destinations = []
+        
+    def addSource(self, source):
+        self.sources.append(source)
+        
+    def addDestination(self, dest):
+        self.destinations.append(dest)
+        
+    def distribute(self):
+        items = []
+        for source in self.sources:
+            items.extend(source.getItems())
+        for dest in self.destinations:
+            dest.receiveItems(items)
             
-    body = server.body(id)[3]
+class NNTPSource:
+    def __init__(self, servername, group, window):
+        self.servername = servername
+        self.group = group
+        self.window = window
+        
+    def getItems(self):
+        start = localtime(time() - self.window*day)
+        date = strftime('%y%m%d', start)
+        hour = strftime('%H%M%S', start)
+
+        server = NNTP(self.servername)
+
+        ids = server.newnews(self.group, date, hour)[1]
+
+        for id in ids:
+            lines = server.article(id)[3]
+            message = message_from_string('\n'.join(lines))
+            
+            title = message['subject']
+            body = message.get_payload()
+            if message.is_multipart():
+                body = body[0]
+                
+            yield NewsItem(title, body)
+            
+        server.quit()
+
+class SimpleWebSource:
+    def __init__(self, url, titlePattern, bodyPattern):
+        self.url = url
+        self.titlePattern = re.compile(titlePattern)
+        self.bodyPattern = re.compile(bodyPattern)
+        
+    def getItems(self):
+        text = urlopen(self.url).read()
+        titles = self.titlePattern.findall(text)
+        bodies = self.bodyPattern.findall(text)
+        for title, body in zip(titles, bodies):
+            yield NewsItem(title, wrap(body))
+ 
+class Destination:
+    def receiveItems(self, items):
+        pass
+        
+class PlainDestination(Destination):
+    def receiveItems(self, items):
+        for item in items:
+            print(item.title)
+            print('-'*len(item.title))
+            print('\n'.join(item.body))
+        
+class HTMLDestination(Destination):
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def receiveItems(self, items):
+        out = open(self.filename, 'w')
+        out.write("""
+        <html>
+            <head>
+                <title>Today's News</title>
+            </head>
+            <body>
+                <h1>Today's News</h1>
+        """)
+        
+        out.write("<ul>")
+        id = 0
+        for item in items:
+            id += 1
+            out.write('<li><a href="#%i">%s</a></li>'%(id, item.title))
+        out.write("</ul>")
+        
+        id = 0
+        for item in items:
+            id += 1
+            out.write('<h2><a href="%i">%s</a></h2>'%(id, item.title))
+            out.write('<pre>%s</pre>'%(item.body))
+            
+        out.write("""
+            </body>
+        </html>    
+        """)
+       
+def runDefaultSetup():
+    agent = NewsAgent()
+    bbc_url = 'http://news.bbc.co.uk/text_only.stm'
+    bbc_title = r'(?s)a href="[^"]*\s*<b>\s*(.*?)\s*</b>">'
+    bbc_body = r'(?s)</a>\s*<br />\s*(.*?)\s*<'
+    bbc = SimpleWebSource(bbc_url, bbc_title, bbc_body)
     
-    print(subject)
-    print('-'*len(subject))
-    print('\n'.join(body))
+    agent.addSource(bbc)
     
-server.quit()
+    nntp_servername = 'news-archive.icm.edu.pl'
+    nntp_group = 'junk'
+    nntp_window = 7
+    nntp_source = NNTPSource(nntp_servername, nntp_group, nntp_window)
+    
+    agent.addSource(nntp_source)
+    
+    agent.addDestination(PlainDestination())
+    agent.addDestination(HTMLDestination('news.html'))
+    
+    agent.distribute()
+    
+if __name__ == "__main__":
+    runDefaultSetup()
